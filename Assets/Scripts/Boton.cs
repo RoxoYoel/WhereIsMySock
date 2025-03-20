9f67c00b69226caa28b99f571857354c7c7a6f75
whisteblower <nachogarciabattista21@gmail.com>
2025-03-20 18:23:29 +0100

    ¡+o

--- Assets/Scripts/Boton.cs
+++ Assets/Scripts/Boton.cs
@@ -4,9 +4,12 @@ public class Boton : MonoBehaviour
 {
     public Animator animBoton;
     public JumpController jumpController;
+
+    //private bool playerInRange = false;
+    public Animator gearAnim;
     void Start()
     {
-        
+       // LeverCanvas.SetActive(false);
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
@@ -14,10 +17,20 @@ public class Boton : MonoBehaviour
         if (collision.gameObject.CompareTag("Boton") && jumpController.jumpAttack)
         {
             animBoton.SetBool("Presionado", true);
+            gearAnim.Play("GearSpin2");
+
+        }
+
+        if (collision.gameObject.CompareTag("Boton2") && jumpController.jumpAttack)
+        {
+            animBoton.SetBool("Presionado", true);
+            Debug.Log("Palanca activada");
+            gearAnim.Play("Gear4");
+
         }
     }
     void Update()
     {
-        
+ 
     }
 }
